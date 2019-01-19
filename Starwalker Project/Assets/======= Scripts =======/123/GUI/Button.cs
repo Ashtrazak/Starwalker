0884df9f7e8c8b773e1b840d08b5dbbf3162fe59
Ashtrazak <45532864+Ashtrazak@users.noreply.github.com>
2019-01-19 15:03:58 +0300

    Проработка перехода между сценами
    Добавлен эффект приближения к кораблю Игрока
    Изменены кнопки интерфейса
    Коректно масштабируется интерфйс на экран телефона
    
    Проведен тест на мобильном устройстве

---
+++ Starwalker Project/Assets/======= Scripts =======/123/GUI/Button.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.EventSystems;
+using System.Collections;
+
+public class Button : MonoBehaviour, IPointerClickHandler
+{
+    [SerializeField] float timeDelay;
+
+    public UnityEvent ClickEvent;
+
+    public void OnPointerClick(PointerEventData evenData)
+    {
+        StartCoroutine(ClickCorutine());
+    }
+
+    private IEnumerator ClickCorutine()
+    {
+        yield return new WaitForSeconds(timeDelay);
+        ClickEvent.Invoke();
+    }
+}
---
+++ Starwalker Project/Assets/======= Scripts =======/123/Player/PlayerSetScale.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+
+public class PlayerSetScale : MonoBehaviour
+{
+    private Transform _transform;
+
+	void Awake ()
+    {
+        _transform = GetComponent<Transform>();
+    }
+
+    public void SetScale(float scale)
+    {
+        _transform.localScale = new Vector3(scale, scale, _transform.localScale.z);
+    }
+}
